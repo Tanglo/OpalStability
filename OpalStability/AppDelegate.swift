13499433dd582e79868eb88f1a84763054e01a7f
Tanglo <Tanglo@users.noreply.github.com>
2015-11-30 14:26:09 +1100

    Initial Commit

---
+++ OpalStability/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  OpalStability
+//
+//  Created by Lee Walsh on 30/11/2015.
+//  Copyright © 2015 Lee David Walsh. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
